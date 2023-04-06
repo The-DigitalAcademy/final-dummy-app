d7c0dbd40531c4b68584b986590c435db4cbdc48
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2023-04-06 08:42:10 +0200

    fix: startPayement

--- js/microapp.js
+++ js/microapp.js
@@ -115,8 +115,8 @@ function onLocationChanged(lat, lon) {
 //PAYMENTS
 function startAyobaPayment(methodId, amountId, currencyId, descriptionId) {
     const method = document.getElementById(methodId).value
-    const amount = document.getElementById(amountId).value
-    const currency = parseFloat(document.getElementById(currencyId).value)
+    const amount = parseFloat(document.getElementById(amountId).value)
+    const currency = document.getElementById(currencyId).value
     const description = document.getElementById(descriptionId).value
 
     const overlay = document.getElementById('input-overlay').checked
